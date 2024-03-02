a4e9eaa1146752a90a82180526119d0195e11345
MahaanManda <135271129+MahaanManda@users.noreply.github.com>
2024-03-02 14:16:07 -0500

    Update solar_eclipse.py
    changed colors and added gridlines

--- solar_eclipse.py
+++ solar_eclipse.py
@@ -4,8 +4,10 @@ import numpy as np
 
 solar_eclipse = pd.read_csv('eclipse_subset.csv')
 
-plt.scatter(solar_eclipse['temp_sun'], solar_eclipse['full_sun_brightness'], label='Full Sun Brightness', marker='o', s=1)
-plt.scatter(solar_eclipse['temp_shade'], solar_eclipse['shade_brightness'], label='Shade Sun Brightness', marker='o', s=1)
+plt.scatter(solar_eclipse['temp_sun'], solar_eclipse['full_sun_brightness'], label='Full Sun Brightness', color='m', s=10)
+plt.scatter(solar_eclipse['temp_shade'], solar_eclipse['shade_brightness'], label='Shade Sun Brightness', color='b', s=10)
+plt.grid(axis = 'y', linestyle=':', color='cyan')
+
 plt.xlabel('Temperature')
 plt.ylabel('Brightness')
 plt.title('Brightness vs Temperature')
